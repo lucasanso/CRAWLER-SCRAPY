96cd09f46a85ce8516415ffb21c1e5a484010a3f
Lucas Santos <146152491+lucasanso@users.noreply.github.com>
2025-12-23 01:59:02 -0300

    Update pipelines.py

--- estadao/estadao/pipelines.py
+++ estadao/estadao/pipelines.py
@@ -1,59 +1,140 @@
-from itemadapter import ItemAdapter
-from scrapy.exporters import JsonItemExporter
-from sshtunnel import open_tunnel                # Para estabelecer a conexão via SSH
-import pymongo                                   # Para criar o cliente de conexão
-import sys                                       # Finalizou o programa: erro (1) ok (0)
-import yaml                                      # Para carregar o arquivo .yaml com as credenciais do MongoDB
-from .items import EstadaoItem                   # Procurar no diretório atual (uso de apenas um [1] ponto
+from sshtunnel import open_tunnel
+import pymongo 
+import sys
+import yaml
 
+from .items import G1Item
 
+# Abre as credenciais do MongoDB que estão no arquivo config.yaml
 try:
-    with open('config.yaml', 'r') as config_file:  # Em 90% dos casos precisa do 'as'.
+    with open('config.yaml', 'r') as configs_file:
         configs = yaml.safe_load(configs_file)
-        
-    except FileNotFoundError:
-        print("Não foi possível encontrar o arquivo.yaml.")
-        
-        # Deu erro, então utiliza-se o número 1.
-        sys.exit(1)
+except FileNotFoundError:
+    print("ERRO CRÍTICO: Arquivo config.yaml não encontrado!")
+    
+    # O programa terminou com erro (com o 0 seria sucesso).
+    sys.exit(1)
 
-
-class EstadaoPipeline:
-    def __init__(self, mongodb_uri, mongodb_database, mongodb_accepted_news_collection, mongodb_unaccepted_news_collection):
-        self.mongodb_uri = mongo_uri
-        self.mongodb_database = mongodb_database
-        self.mongodb_accepted_news_collection = mongodb_accepted_news_collection
-        self.mongodb_unnacepted_news_collection = mongo_unaccepted_news_collection
+# MongoDB LaMCAD
+class MongoDBPipeline:
+    def __init__(self):
+        self.mongodb_uri = configs['mongodb_lamcad']['uri']
+        self.mongodb_database = configs['mongodb_lamcad']['database']
+        self.mongodb_accepted_news_collection = configs['mongodb_lamcad']['accepted_news_collection']
+        self.mongodb_unaccepted_news_collection = configs['mongodb_lamcad']['unaccepted_news_collection']
+        
         self.server = None
         self.client = None
-        
+
+    # Método que inicia o pipeline settando as credenciais do arquivo .yaml no SSH e conectando ao MongoDB.
     def open_spider(self, spider):
+        # Fazendo a conexão ssh com o servidor
         lamcad_configs = configs['lamcad']
-        
         try:
             self.server = open_tunnel(
                 (lamcad_configs['server_ip'], lamcad_configs['server_port']),
                 ssh_username=lamcad_configs['ssh_username'],
-                ssh_password=lamcad_configs['ssh_passoword'],
-                local_bind_adress=(lamcad_configs['local_bind_ip'], lamcad_configs['local_bind_port']),
-                remote_bind_adress=(lamcad_configs['remote_bind_ip'], lamcad_configs['remote_bind_port'])
-                )
+                ssh_password=lamcad_configs['ssh_password'],
+                local_bind_address=(lamcad_configs['local_bind_ip'], lamcad_configs['local_bind_port']),
+                remote_bind_address=(lamcad_configs['remote_bind_ip'], lamcad_configs['remote_bind_port'])
+            )
             self.server.start()
             spider.logger.info(
-                f"Conexão com o MongoDB estabelecida com sucesso {self.server.local_bind_adress}")
-                    
+                f"Conexão com o LamCAD criada com o seguinte IP e porta: {self.server.local_bind_address}")
+
+            # Obtendo acesso ao banco de dados
             self.client = pymongo.MongoClient(self.mongodb_uri)
             database = self.client[self.mongodb_database]
             self.accepted_news_collection = database[self.mongodb_accepted_news_collection]
             self.unaccepted_news_collection = database[self.mongodb_unaccepted_news_collection]
-            
         except Exception as e:
             spider.logger.error(f"Erro crítico ao conectar no banco ou SSH: {e}")
-
-      
+            
+            
+    # Método que interrompe o pipeline.
     def close_spider(self, spider):
         if self.client:
-            self.cliente.close()              # cliente agora tem métodos do PyMongo
-            
+            self.client.close()
         if self.server:
-            self.server.stop()                # server tem métodos do SSH
+            self.server.stop()
+    
+    # Método que insere as notícias aceitas e não aceitas nas respectivas coleções.
+    def process_item(self, item, spider):
+        # Transforma o item Scrapy em um dicionário Python
+        data = dict(G1Item(item))
+        
+        # Verifica se foi aceito pela flag que definimos no scrape.py
+        is_accepted = data.get('accepted_by')
+
+        if is_accepted:
+            self.set_news_data(data)
+            print(f"✅ [MONGODB] Inserindo notícia ACEITA: {data.get('url')}")
+            
+            # Insere na coleção de aceitos com todos os dados
+            self.accepted_news_collection.insert_one(data)
+            
+            # Remove da coleção de não aceitos se já estiver lá (para evitar duplicidade entre coleções)
+            self.unaccepted_news_collection.delete_one({'url': data.get('url')})
+            
+        else:
+            # Verifica se a URL já existe na coleção para não duplicar
+            if not self.unaccepted_news_collection.find_one({'url': data.get('url')}):
+                print(f"🚫 [MONGODB] Salvando na coleção UNACCEPTED (Apenas URL): {data.get('url')}")
+                
+                
+                # Formatação da notícia não aceita: composta pelo id (preenchido automaticamente pelo MongoDB) e pela URL
+                minimal_data = {
+                    'url': data.get('url')
+                }
+                
+                self.unaccepted_news_collection.insert_one(minimal_data)
+            else:
+                # Exibe no terminal se a URL já está na coleção de notícias não aceitas.
+                print(f"⏭️ URL já existe no Unaccepted (Pulando): {data.get('url')}")
+                pass
+                
+        return item
+        
+        
+    # Método que calcula a quantidade de notícias aceitas no banco.
+    def get_accepted_news_count(self):
+        return self.accepted_news_collection.count_documents({}) # sem filtro, ou seja, qualquer coisa escrita vai somar no contador
+        
+    
+    def get_next_id_event(self): 
+        last_record = self.accepted_news_collection.find_one(sort=[('id_event', -1)])
+        
+        if last_record and 'id_event' in last_record:
+            return last_record['id_event'] + 1
+            
+        # é como se tivesse um else aqui, para caso o banco esteja vazio, daí retorna 1.
+        return 1 
+
+
+
+    # Método que adiciona os campos que serão preenchidos via LLM do Davi 
+    def set_news_data(self, news):
+        # Define todos os campos extras como None e insere o id obtido em get_next_id_event.
+        
+        news['manual_relevance_class'] = None
+        news['automatic_relevance_class'] = None
+        news['relevance_model'] = None
+        news['certainty_level'] = None
+        news['relevance_classification_date'] = None
+        news['id_event'] = self.get_next_id_event()
+        news['confidence_relevance_class'] = None
+        news['resumo'] = None
+        news['data_evento'] = None
+        news['quant_mortes'] = None
+        news['tipo_droga'] = None
+        news['quant_droga'] = None
+        news['pais'] = None
+        news['regiao'] = None
+        news['municipio'] = None
+        news['coordenados_GPS'] = None
+        news['tipo_conflito_armado'] = None
+        news['ator1_nome'] = None
+        news['ator1_cod'] = None
+        news['ator2_nome'] = None
+        news['ator2_cod'] = None
+        news['tipo_relacao_entre_atores'] = None
